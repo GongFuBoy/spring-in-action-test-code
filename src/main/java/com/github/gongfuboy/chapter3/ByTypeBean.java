b7fd298346ff5d92987152bd25a324451e6310dc
ZhouLiMing <719630857@qq.com>
2018-01-15 11:28:28 +0800

    chapter3代码提交

---
+++ src/main/java/com/github/gongfuboy/chapter3/ByTypeBean.java
@@ -0,0 +1,26 @@
+package com.github.gongfuboy.chapter3;
+
+/**
+ * @author GongFuBoy
+ * @date 2018/1/15
+ * @time 10:52
+ */
+public class ByTypeBean {
+
+    private Temp1 temp1;
+
+    @Override
+    public String toString() {
+        return "ByTypeBean{" +
+                "temp1=" + temp1 +
+                '}';
+    }
+
+    public Temp1 getTemp1() {
+        return temp1;
+    }
+
+    public void setTemp1(Temp1 temp1) {
+        this.temp1 = temp1;
+    }
+}
